d5121bdbf4891d28878cf85a04d70423c3778092
YoanDieu <yoan.dieu@gmail.com>
2015-10-09 00:07:47 +0200

    added capacity to mark a keyword in pages_controller.rb home.html.erb and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,29 @@
 class PagesController < ApplicationController
   def home
+    @keywords = Keyword.all
   end
 
   def search
     @search = params[:search]
+  end
+
+  def marking
+
+    if KeywordMark.where(:user_id => current_user)
+      keyword_to_update = KeywordMark.where(:user_id => current_user).first
+
+      if keyword_to_update.update note: params[:note]
+        redirect_to '/'
+      else
+
+      end
+    else
+      @marking = KeywordMark.new note: params[:note], keyword_id: params[:keyword_id], user_id: current_user.id
+      if @marking.save
+        redirect_to '/'
+      else
+
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post '/search' => 'pages#search'
 
+  post '/keyword/:id/mark' => 'pages#marking'
+
   root to: "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
